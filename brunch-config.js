b22fbc6a920e55d24a32d3f9f2d71137f8195fc8
Votre nom ou pseudodouardo <edouard.perrin@mac.com>
2017-04-30 17:38:20 +0200

    Ajout de Bootstrap 3.3.7, sass-brunch, pleeease-brunch, babel-brunch

--- brunch-config.js
+++ brunch-config.js
@@ -3,3 +3,24 @@ exports.files = {
   javascripts: {joinTo: 'app.js'},
   stylesheets: {joinTo: 'app.css'}
 };
+
+exports.plugins = {
+  babel: { presets: ['latest', 'stage-0'] },
+  pleeease: {
+    sass: true,
+    autoprefixer: {
+      browsers: ['> 1%'],
+    },
+  },
+};
+
+exports.npm = {
+  globals: {
+    jQuery: 'jquery',
+    $: 'jquery',
+    bootstrap: 'bootstrap',
+  },
+  styles: {
+    bootstrap: ['dist/css/bootstrap.css'],
+  },
+};
